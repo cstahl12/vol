c329088e3e57b9b861b26ba98a444d5ca55d51e8
Chris <cstahl12@gmail.com>
2019-01-23 07:33:06 -0500

    Add files via upload

--- option_model_2.R
+++ option_model_2.R
@@ -10,9 +10,9 @@ library(plotly)
 library(ggthemes)
 
 av_api_key("VOJYOHWHDXCGOTWQ")
-date_from <- "2000-07-01"
-date_price <- "2019-01-18"
-date_strike <- "2019-02-15"
+date_from <- "2002-07-01"
+date_price <- "2019-01-22"
+date_strike <- "2019-04-18"
 
 date_arr <- weekdays(seq(as.Date(date_price),
                          as.Date(date_strike),
@@ -24,8 +24,8 @@ days <- data.frame("Day" = date_arr) %>%
 
 days_sim <- length(days$Day)
 
-ticker_symbol <- "IWM"
-start_price <- 147.85
+ticker_symbol <- "SMH"
+
 num_sim <- 100000
 div <- 0
 
@@ -35,6 +35,7 @@ prices <- tq_get(ticker_symbol,
                  outputsize = "full")
 
 last_price <- prices$adjusted_close[length(prices$adjusted_close)]
+start_price <- 90.19
 
 rets <- prices %>%
   mutate(`return` = log(`adjusted_close` / lag(`adjusted_close`, days_sim))) %>%
@@ -44,7 +45,7 @@ kfit <- regkienerLX(rets$return)
 dist <- data.frame("return" = rkiener4(num_sim, m = 0, g = kfit$coefk4[2], k = kfit$coefk4[3], e = kfit$coefk4[4])) %>%
   mutate(`price` = (start_price * (1 + `return`)) - div)
 
-quantiles <- c(.25, .75)
+quantiles <- c(.3, .7)
 q <- quantile(dist$price, quantiles)
 
 # ggplot(data = dist) +
